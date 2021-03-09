0b14c7fd1f5e80ac525145c687a08773c31d026c
levaniCS <levanisarishvili3105@gmail.com>
2021-03-09 23:32:21 +0400

    Add: bubble sort

---
+++ Sort/bubble_sort.js
@@ -0,0 +1,24 @@
+// Optimized BubbleSort with noSwaps
+//  Time complexity
+//        General: O(N^2)
+//        Nearly sorted array: O(N) (The best case)
+function bubbleSort(arr){
+  let noSwaps;
+  for(let i = arr.length; i > 0; i--){
+    noSwaps = true;
+    for(let j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;         
+      }
+    }
+
+    // Array is already sorteed!
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
